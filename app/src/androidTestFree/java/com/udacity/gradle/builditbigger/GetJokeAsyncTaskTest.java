c5e1c31e5237488bff4f96b78609c9b8b1ec6267
Daniele Orlando <daniele.orlando@gmail.com>
2018-06-12 12:03:11 +0200

    espresso test

--- app/src/androidTestFree/java/com/udacity/gradle/builditbigger/GetJokeAsyncTaskTest.java
+++ app/src/androidTestFree/java/com/udacity/gradle/builditbigger/GetJokeAsyncTaskTest.java
@@ -13,7 +13,10 @@ import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.not;
 
 @RunWith(AndroidJUnit4.class)
 public class GetJokeAsyncTaskTest{
@@ -33,6 +36,7 @@ public class GetJokeAsyncTaskTest{
     @Test
     public void checkJoke() {
         onView(withId(R.id.joke_button)).perform(click());
+        onView(withId(me.danieleorlando.jokeactivity.R.id.jokeTv)).check(matches(not(withText(""))));
     }
 
     @After
--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -46,7 +46,7 @@ public class MainActivity extends AppCompatActivity {
 
         getIdlingResource();
 
-        mIdlingResource.setIdleState(false);
+        mIdlingResource.setIdleState(true);
     }
 
 
